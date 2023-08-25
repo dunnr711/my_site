bbf9357dbc8f46c1e23390dc2fd6a8d699f530e9
RyanSDunn <dunnr@carleton.edu>
2023-08-25 19:50:01 +0200

    fixed some bugs

--- my_site/settings_prod.py
+++ my_site/settings_prod.py
@@ -183,7 +183,7 @@ if USE_SPACES:
     # public media settings
     PUBLIC_MEDIA_LOCATION = 'media-files'
     MEDIA_URL = f'https://{AWS_S3_ENDPOINT_URL}/{PUBLIC_MEDIA_LOCATION}/'
-    DEFAULT_FILE_STORAGE = 'hello_django.storage_backends.PublicMediaStorage'
+    DEFAULT_FILE_STORAGE = 'blog.storage_backends.PublicMediaStorage'
 else:
     STATIC_URL = '/static/'
     STATIC_ROOT = BASE_DIR / 'staticfiles'
